76b3cd4861dbc76ac4f4863bf1f972abac90ff7f
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-08-14 18:12:53 +0530

    add topping

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,7 +3,7 @@ import React from "react";
 const Order = ({ pizza }) => {
   return (
     <div className="container order">
-      <h2>Thank you for your order :)</h2>
+      <h2>Thank you for your order :</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
       {pizza.toppings.map((topping) => (
         <div key={topping}>{topping}</div>
