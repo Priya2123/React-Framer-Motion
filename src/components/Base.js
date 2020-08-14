998b7bd8aae2a69034a0e2ebcb0b2374b1c1d097
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-08-14 18:07:17 +0530

    header

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Base = ({ addBase, pizza }) => {
+  //state
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
