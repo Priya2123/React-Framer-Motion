bcead20361ceaf057977328e3fcb6b58339decbe
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-08-23 01:32:10 +0530

    yoyo: repeating animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,16 +3,17 @@ import { Link } from "react-router-dom";
 import { motion, transform } from "framer-motion";
 
 const buttonVariants = {
-  // visible: {
-  //   //keyframes
-  //   x: [0, -20, 20, -20, 20, 0],
-  //   transition: { delay: 2 },
-  // },
   hover: {
-    //keyframes
-    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1],
+    // keyframes - here limited frames
+    // scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1],
+    scale: 1.1,
     textShadow: "0px 0px 8px rgb(255,255,255)",
     boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      //yoyo allows us to repeat animations - here any number is the no. of keyframes(from original value)
+      yoyo: Infinity,
+      duration: 0.3,
+    },
   },
 };
 
@@ -26,11 +27,7 @@ const Home = () => {
     >
       <h2 animate={{}}>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button
-          variants={buttonVariants}
-          // animate="visible"
-          whileHover="hover"
-        >
+        <motion.button variants={buttonVariants} whileHover="hover">
           Create Your Pizza
         </motion.button>
       </Link>
