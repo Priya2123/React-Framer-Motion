cda743324c4b755a29aad0a0d07df14009db9050
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-09-08 22:31:26 +0530

    Updates

---
+++ precache-manifest.b906641dcceb3077a83e61da359c9612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8157be1f41167e2d1d647452640e22",
+    "url": "/React-Framer-Motion/index.html"
+  },
+  {
+    "revision": "be1cd42eb632e4082cdb",
+    "url": "/React-Framer-Motion/static/css/main.2c0db824.chunk.css"
+  },
+  {
+    "revision": "99970f5ed792247a9748",
+    "url": "/React-Framer-Motion/static/js/2.a412fcc0.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/React-Framer-Motion/static/js/2.a412fcc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be1cd42eb632e4082cdb",
+    "url": "/React-Framer-Motion/static/js/main.d13b3482.chunk.js"
+  },
+  {
+    "revision": "cc5c0aa447f954692a6b",
+    "url": "/React-Framer-Motion/static/js/runtime-main.52c41a1a.js"
+  }
+]);
